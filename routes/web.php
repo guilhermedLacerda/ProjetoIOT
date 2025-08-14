207b6760c748bc52370bc5f11aa25d0e4163c0df
guilhermedlacerda <guilhermedossantoslacerda@gmail.com>
2025-08-14 15:47:19 -0300

    CRUD de Ambiente

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Livewire\Ambiente\AmbienteCreate;
+use App\Livewire\Ambiente\AmbienteEdit;
+use App\Livewire\Ambiente\AmbienteList;
+use App\Models\Ambiente;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('ambiente/create', AmbienteCreate::class)->name('ambiente.create');
+Route::get('ambiente/{id}/edit', AmbienteEdit::class)->name('ambiente.edit');
+Route::get('ambiente/index', AmbienteList::class)->name('ambiente.index'); 
