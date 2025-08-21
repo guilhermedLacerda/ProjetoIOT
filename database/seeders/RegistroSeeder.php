daa44040cbefd0a9fd27f18b43c30f64cd286569
guilhermedlacerda <guilhermedossantoslacerda@gmail.com>
2025-08-21 14:06:41 -0300

    Continuação do seeder de Registro. e alterando a pagina do site

--- database/seeders/RegistroSeeder.php
+++ database/seeders/RegistroSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Registro;
 use App\Models\Sensor;
 use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -34,7 +35,7 @@ class RegistroSeeder extends Seeder
             foreach($sensores as $sensor){
                 $tipo = $sensor->tipo;
 
-                $unidade = $unidadesPorTipo($tipo) ?? '';
+                $unidade = $unidadesPorTipo[$tipo] ?? '';
 
                 switch($tipo){
                     case 'temperatura':
@@ -43,8 +44,25 @@ class RegistroSeeder extends Seeder
                     case 'umidade':
                         $valor = $faker->randomFloat(2, 20, 90);
                         break;
+                    case 'luminosidade':
+                        $valor = $faker->numberBetween(0, 1000);
+                        break;
+                    case 'presenca':
+                        $valor = $faker->randomElement(['ON', 'OFF']);
+                        break;
+                    default:
+                        $valor = $faker->randomFloat(2, 0, 100);
+                        break;
                 }
+
+                Registro::create([
+                    'sensor_id' => $sensor->id,
+                    'valor' => $valor,
+                    'unidade' => $unidade,
+                    'data_hora' => $dataAtual->format('Y-m-d H:i:s')
+                 ]);    
             }
+            $dataAtual->addMinutes(10);
         }
     }
 }
