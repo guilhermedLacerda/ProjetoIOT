a8cc199544686200f451bb259e63ae1ba3bb439b
guilhermedlacerda <guilhermedossantoslacerda@gmail.com>
2025-09-16 13:36:29 -0300

    Crud de registro

---
+++ app/Livewire/Registro/RegistroList.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Livewire\Registro;
+
+use Livewire\Component;
+
+class RegistroList extends Component
+{
+
+    
+
+    public function render()
+    {
+        return view('livewire.registro.registro-list');
+    }
+}
