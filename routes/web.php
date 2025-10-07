819ff5d189f82a1431e5d81499da30e8c26c1ebf
guilhermedlacerda <guilhermedossantoslacerda@gmail.com>
2025-10-07 07:46:45 -0300

    correcao ambiente

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,4 @@ Route::get('ambiente/list', AmbienteList::class)->name('ambiente.list');
 Route::get('ambiente/{id}/delete', AmbienteDelete::class)->name('ambiente.delete'); 
 
 use App\Livewire\Dashboard;
-use Illuminate\Support\Facades\Route;
-
 Route::get('/', Dashboard::class);
